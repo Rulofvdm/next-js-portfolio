3220256651ba14ee94ab6f1cf18ac3aa433d3d11
Rulof van der Merwe <rulofvdm@gmail.com>
2022-10-29 21:28:35 +0200

    Escaped apostrphy for next.js

--- my_portfolio/pages/index.tsx
+++ my_portfolio/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Rulof's Portfolio 📃</title>
+        <title>Rulof&apos;s Portfolio 📃</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
